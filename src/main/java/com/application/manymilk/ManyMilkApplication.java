a80b72d7b97e7c1564c877c7a324aa6080f035d5
vanivice <ltd.vermut@mail.ru>
2025-09-12 14:26:05 +0800

    Initial commit

---
+++ src/main/java/com/application/manymilk/ManyMilkApplication.java
@@ -0,0 +1,13 @@
+package com.application.manymilk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyMilkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManyMilkApplication.class, args);
+	}
+
+}
